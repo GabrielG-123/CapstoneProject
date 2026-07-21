1e488e0ebc3a6e033cc3c65cdcba090e9019e1bf
Anderson Rodriguez <24983183+ShroomTastic4@users.noreply.github.com>
2026-07-20 20:31:08 -0400

    Replace WallData with SurfaceData and update material system

--- Assets/Scripts/Materials/MaterialChanger.cs
+++ Assets/Scripts/Materials/MaterialChanger.cs
@@ -3,14 +3,23 @@ using UnityEngine;
 public class MaterialChanger : MonoBehaviour
 {
     private Renderer wallRenderer;
+    private SurfaceData data;
 
     void Awake()
     {
         wallRenderer = GetComponentInChildren<Renderer>();
+        data = GetComponent<SurfaceData>();
+    }
+
+    void Start() 
+    {
+        //Debug.Log($"Height: {data.getHeight()}\n Length: {data.getLength()}");
+
+        wallRenderer.material.mainTextureScale = new Vector2(data.Length, data.Width);
     }
 
     public void ChangeMaterial(Material newMaterial)
-    {
+    {   
         wallRenderer.material = newMaterial;
     }
 }
